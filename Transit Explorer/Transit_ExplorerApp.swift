fcef577e6d593a9067617b024d1314019507e973
Simon Goldring <sbjgoldring@gmail.com>
2021-03-28 12:28:03 +0100

    Initial Commit

---
+++ Transit Explorer/Transit_ExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Transit_ExplorerApp.swift
+//  Transit Explorer
+//
+//  Created by Simon Goldring on 3/28/21.
+//
+
+import SwiftUI
+
+@main
+struct Transit_ExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
